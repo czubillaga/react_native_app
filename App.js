c4150f1062958c2ce8a9fd1c730c94f729db89a9
carloszubillaga <car.zubillaga@gmail.com>
2021-11-28 13:46:59 +0000

    added second fade in

--- App.js
+++ App.js
@@ -1,28 +1,58 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState, useEffect, useRef }from 'react';
-import { StyleSheet, Text, View, ScrollView, Button, Pressable, Animated } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Button, Pressable, Animated, ActivityIndicator } from 'react-native';
 
 export default function App() {
     
-    const [buttonPressed, setButtonPressed] = useState(null)
+    const [buttonPressed, setButtonPressed] = useState(0)
     const [buttonPressString, setButtonPressString] = useState('')
     const fadeAnim = useRef(new Animated.Value(0)).current
+    const fadeAnim2 = useRef(new Animated.Value(0)).current
+    const fadeAnim3 = useRef(new Animated.Value(0)).current
+
     
 //    const handleButtonPress = () => {
 //        setButtonPressString('Wow, a button that does something!')
 //    }
     
     const fadeIn = () => {
-        Animated.timing(fadeAnim, {
+        Animated.sequence([
+            Animated.timing(fadeAnim, {
+                toValue: 1,
+                duration: 3000,
+                useNativeDriver: true
+            }),
+            Animated.timing(fadeAnim2, {
+                toValue: 1,
+                duration: 3000,
+                useNativeDriver: true
+            })
+        ]).start()
+        
+    }
+    
+    const secondFadeIn = () => {
+        Animated.timing(fadeAnim3, {
             toValue: 1,
-            duration: 5000
+            duration: 3000,
+            useNativeDriver: true
         }).start()
+    }
+    
+    const clickCount = () => {
+        setButtonPressed(buttonPressed + 1)
+    }
+    
+    const checkClickCount = () => {
+        if(buttonPressed === 1) {
+            secondFadeIn()
+        }
     }
     
     
   return (
     <ScrollView style={styles.scrollView}>
-    <View style={styles.container}>
+          <View style={[styles.container]}>
           <Text style={styles.text}>This is Carlos editing App.js and getting it to build successfully on an iOs app simulator using Xcode and React Native. He is very proud.
           </Text>
           <Text></Text>
@@ -32,10 +62,14 @@ export default function App() {
           <Text></Text>
           <Text style={styles.text}>Now, press {'this'} button</Text>
           <Pressable style={styles.button} >
-          <Button style={styles.buttonText} title="Press it..." onPress={fadeIn}/>
+          <Button style={styles.buttonText} title="Press it..." onPress={() => {fadeIn(); clickCount(); checkClickCount();}}/>
           </Pressable>
           <Text></Text>
           <Animated.Text style={[styles.text, {opacity: fadeAnim}]}>Wow, a button that does something!</Animated.Text>
+          <Text></Text>
+          <Animated.Text style={[styles.text, {opacity: fadeAnim2}]}>Press it again...</Animated.Text>
+          <Text></Text>
+          <Animated.Text style={[styles.text, {opacity: fadeAnim3}]}>Amazing.</Animated.Text>
       <StatusBar style="auto" />
     </View>
     </ScrollView>
